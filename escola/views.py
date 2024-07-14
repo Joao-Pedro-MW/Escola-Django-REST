c53b859363826377954897850ee78751ae1d3728
João Pedro MW <jpedro01230@gmail.com>
2024-07-14 15:11:19 -0300

    Primeira rota e mais configurações

--- escola/views.py
+++ escola/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+# só criaremos a API
+
+def estudantes(request):
+    if request.method == 'GET':
+        estudante = {
+            'id':'1',
+            'nome':'Joao'
+        }
+        return JsonResponse(estudante)
